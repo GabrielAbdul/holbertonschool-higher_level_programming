5f04041c748bf3050eecb614822931a8e26f8074
Gabriel <gabrielabdul1@gmail.com>
2020-04-28 19:48:06 +0000

    removed extra 99 printed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(100):
+for i in range(0, 99):
     print("{:02d}, ".format(i), end="")
-    if i == 99:
-        print("{:02d}".format(i))
+    if i + 1 == 99:
+        print("{:02d}".format(99))
