f0f7e17b2ab9b104cbef8c2dbbca23c05358e203
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-05 23:32:39 +0000

    checked for idx equalling 0 and added len - 1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,11 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
     for i in my_list:
+        if idx == 0:
+            my_list[idx] = element
         if i == idx:
             my_list[idx] = element
     return my_list
