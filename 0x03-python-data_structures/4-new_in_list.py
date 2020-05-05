8c616d7959d5fb7133062c4493b0bc9538c48b1f
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-05 23:43:47 +0000

    checked for NULL in my_list, add idx check for 0

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,14 @@
 
 
 def new_in_list(my_list, idx, element):
+    if not my_list:
+        return None
     if idx < 0 or idx > len(my_list):
         return (my_list)
     new_list = my_list[:]
     for i in new_list:
+        if idx == 0:
+            new_list[idx] = element
         if i == idx:
             new_list[idx] = element
     return new_list
