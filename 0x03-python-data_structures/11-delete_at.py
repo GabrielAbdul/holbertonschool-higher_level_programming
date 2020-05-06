d7d13d37c4eb65a7d8e12197193dba3cc1df62c7
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-06 03:40:36 +0000

    added idx check

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,10 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if not my_list or not my_list[idx]:
+    if not my_list
+        return my_list
+    list_len = len(my_list)
+    if idx > list_len:
         return my_list
     my_list[idx:idx + 1] = []
     return my_list
