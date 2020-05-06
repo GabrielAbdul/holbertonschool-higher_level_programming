460c1ea6594a80bc36b4306d19b5478263502349
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-06 03:05:09 +0000

    added variable names and pep8 fixes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,6 +3,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     nums, nums_a, nums_b = [0, 0], [0, 0], [0, 0]
+
     if not tuple_a and not tuple_b:
         return None
     if len(tuple_a) == 1:
@@ -16,7 +17,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(0, 2):
         nums_a[i] = tuple_a[i]
         nums_b[i] = tuple_b[i]
-    nums[0] = nums_a[0] + nums_b[0]
-    nums[1] = nums_a[1] + nums_b[1]
-    return tuple(nums)
-
+    add_one = nums_a[0] + nums_b[0]
+    add_two = nums_a[1] + nums_b[1]
+    result = (add_one, add_two)
+    return result
