8e07e4e1fd1a6afa0edcb1da359efb0619644508
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-06 00:01:25 +0000

    if not sentence none will get added

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,7 +5,9 @@ def multiple_returns(sentence):
     my_list = [0, 0]
     if not sentence:
         fst_chr = None
+    elif sentence:
+        fst_chr = sentence[0]
     my_list[0] = len(sentence)
-    my_list[1] = sentence[0]
+    my_list[1] = fst_chr
 
     return tuple(my_list)
