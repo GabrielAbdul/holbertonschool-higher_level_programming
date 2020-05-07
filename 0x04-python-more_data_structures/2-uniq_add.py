091770b38e7a366c81468542e0dd712c6d8b7f3d
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-07 19:19:13 +0000

    initial commit

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+
+    sum = 0
+
+    new_list = list(set(my_list))
+
+    for i in range(len(new_list)):
+
+        sum += int(new_list[i])
+    return sum
