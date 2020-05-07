195bfd782c124c5b009437ab96e3fdf201753afa
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-07 20:15:42 +0000

    initial commit

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin?python3
+
+
+def print_sorted_dictionary(a_dictionary):
+
+    list_keys = sorted(a_dictionary)
+
+    for i in range(len(list_keys)):
+
+        print("{}: {}".format(list_keys[i], a_dictionary[list_keys[i]]))
