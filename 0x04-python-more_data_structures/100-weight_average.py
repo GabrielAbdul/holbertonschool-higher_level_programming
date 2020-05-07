665c799654e24a1c8c9af51a1c052ac501775305
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-07 23:20:09 +0000

    initial commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+
+    if not my_list:
+        return 0
+
+    # multiplies individual tuples together
+    for tuples in my_list:
+        elements = list(map(lambda x: x[0] * x[1], my_list))
+
+    dividend = 0
+    for tuples in my_list:
+        dividend += tuples[1]
+
+    divisor = 0
+    for elem in elements:
+        divisor += elem
+
+    result = divisor / dividend
+    
+    return result
