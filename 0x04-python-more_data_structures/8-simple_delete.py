06cdd257c4afee29bcf0a2577d2dcbf953022826
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-07 20:52:37 +0000

    used del

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 
-def simple_delete(a_dicitonary, key=""):
+def simple_delete(a_dictionary, key=""):
 
-    a_dictionary.pop(key)
+    if key in a_dictionary:
+
+        del a_dictionary[key]
 
     return a_dictionary
