b943775bb413de4e35f86f0d14c9172262daf62b
GabrielAbdul <gabrielabdul1@gmail.com>
2020-06-03 18:24:21 +0000

    initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """read_file()
+        Args:
+            filename - name of the file
+    """
+    with open(filename, encoding='UTF-8') as file:
+        print(file.read())
