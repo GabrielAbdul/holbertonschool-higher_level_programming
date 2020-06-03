825e0dae56ec759339bf5586a632e6a6290d0452
GabrielAbdul <gabrielabdul1@gmail.com>
2020-06-03 22:39:48 +0000

    initial commit

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,4 +9,14 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 from sys import argv
 
 try:
+    arg_list = load_from_json_file('add_item.json')
+    arg_list = list(arg_list)
+except Exception:
+    arg_list = []
+    save_to_json_file(arg_list, 'add_item.json')
+
+for args in range(1, len(argv)):
+    arg_list.append(argv[args])
+
+save_to_json_file(my_list, 'add_item.json')
         
