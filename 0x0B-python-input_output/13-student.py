70a378bab2ba167b078a40da32bf8296ebd8f0ca
gabrielabdul <gabrielabdul1@gmail.com>
2020-06-04 17:59:55 +0000

    inital commit

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """
+        Attributes:
+            first_name - first name
+            last_name - last name
+            age - age
+        Methods:
+            to_json() - retrieves a dict repr of a Student instance
+    """
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json() - retrieves a dict repr of a Student instance """
+        list_attrs = {}
+        if type(attrs) is not list or attrs is None:
+            return self.__dict__
+        for key in attrs:
+            if type(key) is not str:
+                return self.__dict__
+            if key in self.__dict__:
+                list_attrs[key] = self.__dict__[key]
+        return list_attrs
+    def reload_from_json(self, json):
+        """reload_from_json
+            loops through all keys in json dictionary,
+            and replaces all attributes in self class
+            with attributes found in dict
+        """
+        for attr in json.keys():
+            setattr(self, attr, json[attr])
