4325f26f8837b49830857234ca063a6093c32834
Gabriel <gabrielabdul1@gmail.com>
2020-06-10 02:42:38 +0000

    Updated to JSON handling

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """1. Base Class """
 
 
+import json
+
+
 class Base:
     """Class Base
         Attributes:
@@ -20,3 +23,32 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representatin of list_dictionaries
+            Args:
+                list_dictionaries - list of dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation to a file
+            Args:
+                cls - class name
+                list_objs - list of instances that inherit from Base
+        """
+        list_dicts = []
+        for instances in list_objs:
+            list_dicts.append(instances.to_dictionary())
+        json_dicts = Base.to_json_string(list_dicts)
+        try:
+            with open("{}.json".format(cls.__name__), 'a') as jason:
+                jason.write(json_dicts)
+        except FileNotFoundError:
+            with open("{}.json".format(cls.__name__), 'x') as jason:
+                jason.write(json_dicts)
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from class Rectangle
+        Attributes:
+            size - size
+            x - x
+            y - y
+        Methods:
+            __init__() - Class constructor
+"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """Size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Size setter"""
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        elif size <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = size
+            self.__height = size
+            self.__size = size
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        if args:
+            try:
+                self.id = args[0]
+            except Exception:
+                pass
+            try:
+                self.size = args[1]
+            except Exception:
+                pass
+            try:
+                self.x = args[2]
+            except Exception:
+                pass
+            try:
+                self.y = args[3]
+            except Exception:
+                pass
+        else:
+            for keys in kwargs.keys():
+                setattr(self, keys, kwargs[keys])
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        self_dict = {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
+        return self_dict
+
+    def __str__(self):
+        """returns [Square] (<id>) <x>/<y> - <size>"""
+        return "Square ({}) {}/{} - {}\
+".format(self.id, self.x, self.y, self.width)
