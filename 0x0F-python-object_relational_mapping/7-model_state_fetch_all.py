dfe4f95497ed35dae6f5da2520a88d6f812c864f
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-08 18:56:35 +0000

    removed sys.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     from sqlalchemy.orm import sessionmaker
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
-                           '.format(sys.argv[1], sys.argv[2],
-                           sys.argv[3]), pool_pre_ping=True)
+                           '.format(argv[1], argv[2],
+                           argv[3]), pool_pre_ping=True)
 
     # need to create a session
     Session = sessionmaker(bind=engine)
