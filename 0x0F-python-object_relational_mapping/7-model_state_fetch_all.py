a009046fadb6716beef91abf8dd48399631c92a2
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-08 18:06:56 +0000

    removed redudancy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,8 +13,7 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
                            '.format(sys.argv[1], sys.argv[2],
                            sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
+    
     # need to create a session
     Session = sm(bind=engine)
     session = Session()
