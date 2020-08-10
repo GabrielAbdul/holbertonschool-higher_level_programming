0d5c7b9386c2d4c05938ed33532fea8b8da8e05a
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-10 01:08:53 +0000

    fixed formatting of continuation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,9 +8,8 @@ if __name__ == '__main__':
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
-                           '.format(argv[1], argv[2],
-                           argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     # need to create a session
     Session = sessionmaker(bind=engine)
