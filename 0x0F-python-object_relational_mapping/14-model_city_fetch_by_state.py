1ea12932f6e438f613ece7976fc87682a1eaf6a1
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-16 16:09:51 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Get all states that match input state, safe from injection1'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker as sm
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # define parameters for session
+    Session = sm(bind=engine)
+
+    # create session
+    session = Session()
+    for c, s in session.query(City, State).filter(City.state_id == State.id)
+                                          .order_by(City.id).all():
+        print("{} ({}) {}".format(s.name, c.id, c.name))
+
+session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Cities in state'''
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    '''class City inherits from Base'''
+
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True,
+                primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
