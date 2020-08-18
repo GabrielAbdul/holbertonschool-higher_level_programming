bbbd99c261552dba2f36bb4826734cec6dca82ec
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-18 17:13:13 +0000

    made comment one line for file line count

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-# takes in a URL, sends a DELETE request to that URL
-# and displays the body of the response
+# takes in a URL, sends a DELETE request to that URL and displays the body of the response
 curl -sX DELETE "$1" ; echo ""
