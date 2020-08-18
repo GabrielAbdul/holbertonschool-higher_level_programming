87db4492ad7d8a9bed1d1f74fb6e5bea5d72f92f
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-18 17:24:18 +0000

    corrected shebang

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # takes in a URL, sends a get request to that URL and displays the body of the response
 curl -sL "$1" 
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # takes in a URL, sends a request to that URL and displays the size of the body of the response
 curl "$1" -sX POST -F "email: hr@holbertonschool.com" -F "subject: I will always be here for PLD"
