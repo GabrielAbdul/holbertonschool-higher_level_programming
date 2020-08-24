0a98d910b0f3e31cab32282bc66d5af569d8257f
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-24 19:13:59 +0000

    changed to if else

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,9 +8,8 @@ if __name__ == '__main__':
     import requests
     from sys import argv
 
-    try:
-        response = requests.get(argv[1])
-        if response.status_code >= 400:
-            print('Error code: {:d}'.format(response.status_code))
-    except:
+    response = requests.get(argv[1])
+    if response.status_code >= 400:
+        print('Error code: {:d}'.format(response.status_code))
+    else:
         print(response.text)
