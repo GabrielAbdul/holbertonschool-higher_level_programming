8a4d419f6be3d032b8b9e61c0631f8abce14fd2c
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-24 21:29:53 +0000

    pep8

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     from sys import argv
 
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
-    re = requests.get(url)
+    r = requests.get(url)
     try:
         for i in range(0, 10):
-            print('{}: {}'.format(re.json()[i]['sha'],
-                                  re.json()[i]['commit']['author'].get('name')))
+            print('{}: {}'.format(r.json()[i]['sha'],
+                                  r.json()[i]['commit']['author'].get('name')))
