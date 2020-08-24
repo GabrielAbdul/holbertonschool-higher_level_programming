b5fa852ef037be19ac393038b0e76a53abd52ee7
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-24 21:25:55 +0000

    switched param args

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     import requests
     from sys import argv
 
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     re = requests.get(url)
     for i in range(0, 10):
         print('{}: {}'.format(re.json()[i]['sha'],
-                              re.json()[i]['commit']['author'].get('name')))
+                              re.json()[i]['commit']['author']['name']))
