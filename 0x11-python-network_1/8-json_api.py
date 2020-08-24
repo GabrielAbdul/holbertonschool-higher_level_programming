03d8771526c71cb68698626a467462f19b6e2127
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-24 17:53:07 +0000

    corrected filename

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter'''
+
+
+if __name__ == '__main__':
+
+    import requests
+    from sys import argv
+
+    url = 'http://0.0.0.0:5000/search_user'
+    try:
+        letter = argv[1]
+        re = requests.post(url, data={'q': letter})
+        if re.json() is not {}:
+            print("[{}] {}".format(re.json()['id'], re.json()['name']))
+    except IndexError:
+        re = requests.post(url, data={'q': ''})
+        if re.json() == {}:
+            print('No result')
