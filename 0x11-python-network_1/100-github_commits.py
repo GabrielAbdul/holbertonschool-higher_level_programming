ec742107a3ae301e673a8bb06b8fa2fe31c89741
GabrielAbdul <gabrielabdul1@gmail.com>
2020-08-24 21:31:04 +0000

    removed try

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
 
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     r = requests.get(url)
-    try:
-        for i in range(0, 10):
-            print('{}: {}'.format(r.json()[i]['sha'],
-                                  r.json()[i]['commit']['author'].get('name')))
+    for i in range(0, 10):
+        print('{}: {}'.format(r.json()[i]['sha'],
+                              r.json()[i]['commit']['author'].get('name')))
