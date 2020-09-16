af826d958ee6573cb1a41f9e0550907dfc74752c
GabrielAbdul <gabrielabdul1@gmail.com>
2020-09-16 00:00:58 +0000

    initial commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (process.argv.length === 2) {
+  if (process.argv.length > 2) {
+    if (process.argv.length > 3) {
+      console.log('Arguments found');
+    }
+    console.log('Argument found');
+  }
+  console.log('No argument');
+}
