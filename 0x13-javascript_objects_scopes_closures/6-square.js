37c7423b974e1d7bcd26e8a208f7bd6456746add
gabrielabdul <gabrielabdul1@gmail.com>
2020-09-22 04:51:19 +0000

    removed constructor imported right file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (!c) {
       return super.print('X');
